ff8c88237b3d63572d03e6443bfce069370c4ee8
adibsurani <adib.surani@gmail.com>
2017-07-02 17:26:12 +1000

    Split window into ShellView and TextEditorView

---
+++ src/Kuriimu/ViewModels/ShellViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Kuriimu.ViewModels
+{
+    class ShellViewModel : Conductor<IScreen>.Collection.OneActive
+    {
+        public ShellViewModel()
+        {
+            DisplayName = "Kuriimu";
+            OpenButton();
+        }
+
+        public void OpenMenu()
+        {
+            ActivateItem(new TextEditorViewModel { DisplayName = "Opened from Menu!" });
+        }
+
+        public void OpenButton()
+        {
+            ActivateItem(new TextEditorViewModel());
+        }
+
+        public void CloseItem(TextEditorViewModel vm)
+        {
+            vm.TryClose();
+        }
+    }
+}
---
+++ src/Kuriimu/ViewModels/TextEditorViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.Collections.ObjectModel;
+
+namespace Kuriimu.ViewModels
+{
+    class TextEditorViewModel : Screen
+    {
+        public TextEditorViewModel()
+        {
+            DisplayName = "Text Editor";
+            AddEntry();
+            SelectedEntry = Entries.First();
+        }
+
+        public class Entry
+        {
+            public string Label { get; set; }
+            public string Text { get; set; }
+            public Entry(string label, string text)
+            {
+                Label = label;
+                Text = text;
+            }
+        }
+
+        public ObservableCollection<Entry> Entries { get; set; } = new ObservableCollection<Entry>();
+
+        Entry _selectedEntry;
+        public Entry SelectedEntry
+        {
+            get => _selectedEntry;
+            set
+            {
+                _selectedEntry = value;
+                EntryText = _selectedEntry.Text;
+                NotifyOfPropertyChange(() => SelectedEntry);
+            }
+        }
+
+        public string EntryText
+        {
+            get => _selectedEntry?.Text;
+            set
+            {
+                if (_selectedEntry == null) return;
+                _selectedEntry.Text = value;
+                NotifyOfPropertyChange(() => EntryText);
+            }
+        }
+
+        public void AddEntry()
+        {
+            Entries.Add(new Entry($"Label {Entries.Count}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")));
+        }
+    }
+}
